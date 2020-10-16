f9c209100f9dd4042188b9fd42732f95b5c3030a
MoonMan42 <34987599+MoonMan42@users.noreply.github.com>
2020-10-16 00:00:04 -0400

    Add project files.

---
+++ WhiteNoise_Xamarin/WhiteNoise_Xamarin/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿
+using Plugin.SimpleAudioPlayer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+namespace WhiteNoise_Xamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        // added Xam.Plugin.SimpleAudioPlayer in nuget
+        //https://devblogs.microsoft.com/xamarin/adding-sound-xamarin-forms-app/
+
+        ISimpleAudioPlayer player;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            player = CrossSimpleAudioPlayer.Current;
+        }
+
+        private void StartPlay_Click(object sender, EventArgs e)
+        {
+            var assembly = typeof(App).GetTypeInfo().Assembly;
+            Stream audioStream = assembly.GetManifestResourceStream("WhiteNoise_Xamarin." + "WhiteOceanNoise.mp3");
+
+            player.Load(audioStream);
+
+            player.Play();
+        }
+
+        private void StopPlay_Click(object sender, EventArgs e)
+        {
+            player.Stop();
+        }
+    }
+}
